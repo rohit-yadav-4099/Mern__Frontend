7f86aa1885e2946ac575b86bf163b2f242769f42
rohit-yadav-4099 <rohitjnp2194@gmail.com>
2024-01-24 15:02:37 +0530

    done

--- src/Redux/MyCourse.js
+++ src/Redux/MyCourse.js
@@ -10,7 +10,14 @@ const AddMyCourse = () => {
         <>
             <div className="MyDashboard">
                 <div className="sidebar">
-                    sidebar
+                    <div className="Profile_nav_selected ">
+                        <span className="Profile_nav-list-icon">
+                            <svg className="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+                                <path d="M5 13.18v2.81c0 .73.4 1.41 1.04 1.76l5 2.73c.6.33 1.32.33 1.92 0l5-2.73c.64-.35 1.04-1.03 1.04-1.76v-2.81l-6.04 3.3c-.6.33-1.32.33-1.92 0L5 13.18zm6.04-9.66l-8.43 4.6c-.69.38-.69 1.38 0 1.76l8.43 4.6c.6.33 1.32.33 1.92 0L21 10.09V16c0 .55.45 1 1 1s1-.45 1-1V9.59c0-.37-.2-.7-.52-.88l-9.52-5.19a2.04 2.04 0 00-1.92 0z"></path>
+                            </svg>
+                        </span>
+                        <span className="Profile_nav-list-detail">My Courses</span>
+                    </div>
                 </div>
 
                 <div className="Mycourse">
